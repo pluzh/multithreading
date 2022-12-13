0d24ee7fa6a2d5a820e73b63f997b88e40bdb90f
Ангелина Плужникова <angelinapluznikova@MacBook-Air-Angelina.local>
2022-12-13 19:21:51 +0500

    final

---
+++ Form1.Designer.cs
@@ -0,0 +1,148 @@
+﻿namespace Лабораторная1_семестр2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Требуется переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Обязательный метод для поддержки конструктора - не изменяйте
+        /// содержимое данного метода при помощи редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(202, 124);
+            this.button1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(155, 52);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Генерировать массив";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(35, 80);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(159, 245);
+            this.textBox1.TabIndex = 1;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(365, 80);
+            this.textBox2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(155, 245);
+            this.textBox2.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft YaHei", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(22, 42);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(193, 19);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Сгенерированный массив";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft YaHei", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(350, 42);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(192, 19);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Отсортированный массив";
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.checkBox1.Location = new System.Drawing.Point(141, 364);
+            this.checkBox1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(286, 23);
+            this.checkBox1.TabIndex = 5;
+            this.checkBox1.Text = "Добавить критические секции в потоки";
+            this.checkBox1.UseVisualStyleBackColor = false;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(202, 194);
+            this.button2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(155, 37);
+            this.button2.TabIndex = 6;
+            this.button2.Text = "Сортировать массив";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 19F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ClientSize = new System.Drawing.Size(576, 418);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.Font = new System.Drawing.Font("Microsoft YaHei", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Name = "Form1";
+            this.Text = "Лабораторная работа №1 Плужниковой А. С. БПС-215";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,154 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+
+namespace Лабораторная1_семестр2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        public static Semaphore smf1 = new Semaphore(0, 1); //создание семафоров; данный смафор для синхронизации вывода в основном потоке
+        public static Semaphore smf2 = new Semaphore(0, 1);// данный смафор для синхронизации вывода в основном потоке
+        public static Semaphore smf = new Semaphore(0, 2); //данный семафор для одновременного запуска двух сортировок
+
+        static object krit = new object(); //создаем объект для критической секции
+    
+        static void Puzirok() //с критичской секцией
+        {
+            smf.Release(); //высвобождает семафор
+            smf.WaitOne();  // ждет, когда в семаформе освободится место и начинает выполнять все дальнейшие действия
+            
+            lock (krit) //собственно критическая секция
+            {
+                for (int i = 0; i < A1.Length; i++)
+                    for (int j = i + 1; j < A1.Length; j++)
+                        if (A1[i] > A1[j])
+                        {
+                            int temp = A1[i];
+                            A1[i] = A1[j];
+                            Thread.Sleep(100);
+                            A1[j] = temp;
+                        }
+            }
+            
+            smf1.Release(); //высвобождает (открывает) семафор для основного потока
+        }
+        static void Puzirok2() // без крит секции
+        {
+            smf.WaitOne();// ждет, когда в семаформе освободится место и начинает выполнять все дальнейшие действия
+            smf.Release();//высвобождает семафор
+            for (int i = 0; i < A1.Length; i++)
+                for (int j = i + 1; j < A1.Length; j++)
+                    if (A1[i] > A1[j])
+                    {
+                            int temp = A1[i];
+                            A1[i] = A1[j];
+                            Thread.Sleep(100);
+                            A1[j] = temp;
+                    }
+
+            smf1.Release();
+        }
+        static void Maksimum()
+            {
+                smf.Release(); 
+                smf.WaitOne();
+                lock (krit)
+                {
+                    for (int i = A1.Length - 1; i >= 1; i--)
+                    {
+                        int max = i;
+                        for (int j = i - 1; j >= 0; j--)
+                        {
+
+                            if (A1[max] < A1[j])
+                            {
+                                max = j;
+                            }
+                        }
+
+                        int temp = A1[i];
+                        Thread.Sleep(100);
+                        A1[i] = A1[max];
+                        A1[max] = temp;
+
+                    }
+                }
+            
+            smf2.Release(); 
+        }
+        static void Maksimum2()
+        {
+            smf.Release();
+            smf.WaitOne();
+
+            for (int i = A1.Length - 1; i >= 1; i--)
+            {
+                int max = i;
+                for (int j = i - 1; j >= 0; j--)
+                {
+
+                    if (A1[max] < A1[j])
+                    {
+                        max = j;
+                    }
+                }
+                    int temp = A1[i];
+                    Thread.Sleep(100);
+                    A1[i] = A1[max];
+                    A1[max] = temp;
+            }
+
+
+            smf2.Release();
+        }
+        public static int[] A1 = new int[10];
+        private void button1_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = "";
+            Random rand = new Random();
+            for (int i = 0; i < A1.Length; i++)
+            {
+                A1[i] = rand.Next(-100,100);
+                textBox1.Text = textBox1.Text + A1[i] + "\r\n";
+            }
+                
+        }
+         private void button2_Click(object sender, EventArgs e)
+        {
+            textBox2.Text = "";
+            if (checkBox1.Checked == false)
+            {
+                Thread potok1 = new Thread(Puzirok2);
+                Thread potok2 = new Thread(Maksimum2);
+                potok1.Start();
+                potok2.Start();
+            }
+            else
+            {
+                Thread potok1 = new Thread(Puzirok);
+                Thread potok2 = new Thread(Maksimum);
+                potok1.Start();
+                potok2.Start();
+            }
+            
+            smf1.WaitOne();
+            smf2.WaitOne();
+
+            for (int i = 0; i < A1.Length; i++)
+            {
+                textBox2.Text = textBox2.Text + A1[i] + "\r\n";
+            }         
+        }
+    }
+}
